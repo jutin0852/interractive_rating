bc248435b38c96cb8f4a719db80c63b7236c4e76
jutin0852 <jutindikonu8@gmail.com>
2022-10-18 21:45:57 +0100

    adding styles

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var rates=document.getElementsByClassName('rate');
 var second=document.getElementById('second');
 var first=document.getElementById('first');
 var current=document.getElementsByClassName('active');
+var five=document.getElementById('five')
 for(var x=0; x<rates.length; x++){
     rates[x].addEventListener('click',function(e){
         if(current.length>0){
@@ -15,5 +16,6 @@ function submit(){
     if(current.length>0){
         first.style.display="none";
         second.style.display="block";
+        five.innerHTML="you selected "+current[0].textContent+" out of 5"
     }
 }
