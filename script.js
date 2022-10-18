b09e3b83473512b55636abd8a0f25b1f3195f015
jutin0852 <jutindikonu8@gmail.com>
2022-10-18 20:56:59 +0100

    added submit function

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var rate=document.getElementById('rate');
 var rates=document.getElementsByClassName('rate');
- for(var x=0; x<rates.length; x++){
-    rates[x].addEventListener('click',function(){
-        var current=document.getElementsByClassName('active');
+var second=document.getElementById('second');
+var first=document.getElementById('first');
+var current=document.getElementsByClassName('active');
+for(var x=0; x<rates.length; x++){
+    rates[x].addEventListener('click',function(e){
         if(current.length>0){
-        current[0].className=current[0].className.replace(" active","");}
-        this.className+=" active";
-});
+            current[0].className=current[0].className.replace(" active","")
+        }
+        e.target.className+=" active"
+    })
+}
+function submit(){
+    if(current.length>0){
+        first.style.display="none";
+        second.style.display="block";
+    }
 }
